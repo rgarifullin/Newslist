33a3f30a245f7596a0196016a052d4a3075aa8df
Rinat <ringarifullin@gmail.com>
2016-02-28 22:21:41 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+news1 = News.create(
+  author: 'Fox',
+  text: 'Britain scientists developed new amazing technology.'
+)
+news2 = News.create(
+  author: 'Smith',
+  text: 'New film about agent 007 was released.'
+)
+
+admin = User.create(
+  email: 'admin@newslist.com',
+  password: 'nicepasswd',
+  role: 1
+)
+viewer = User.create(
+  email: 'viewer@newslist.com',
+  password: 'badpasswd',
+  role: 0
+)
+
+Newsuser.create(user_id: admin.id, news_id: news1.id, read: true)
+Newsuser.create(user_id: admin.id, news_id: news2.id)
+Newsuser.create(user_id: viewer.id, news_id: news1.id)
+Newsuser.create(user_id: viewer.id, news_id: news2.id, read: true)
