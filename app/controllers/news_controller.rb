e9394942b0f78e27e38c9d013cb7d056abb21b31
Rinat <ringarifullin@gmail.com>
2016-02-28 22:48:27 +0300

    Add statistics to index view

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,6 +2,17 @@ class NewsController < ApplicationController
   def index
     @newslist = News.all
     @total = News.count
+
+    return unless current_user
+
+    @total_readed = Newsuser.where(user_id: current_user, read: true).count
+    today_news = News.where(created_at: Date.current..Date.current + 1)
+    @today = today_news.size
+    @readed_today = 0
+    today_news.each do |news|
+      @readed_today += 1 if news.newsusers.find_by(read: true,
+                                                   user_id: current_user)
+    end
   end
 
   def new
@@ -22,7 +33,7 @@ class NewsController < ApplicationController
 
   private
 
-  def news_params()
+  def news_params
     params.require(:news).permit(:author, :text)
   end
 end
