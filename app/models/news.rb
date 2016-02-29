ed91d8ba2b11f33e8ca53ab51d57de206d3a33b5
Rinat <ringarifullin@gmail.com>
2016-02-29 12:45:33 +0300

    Add validations for news model

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,4 +1,7 @@
 class News < ActiveRecord::Base
   has_many :newsusers
   has_many :users, through: :newsusers
+
+  validates :author, presence: true, length: { minimum: 5 }
+  validates :text, presence: true, length: { minimum: 15 }
 end
