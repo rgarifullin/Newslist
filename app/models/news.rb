f712e9e43ce4188fdf050bacb97ee183d50e3ee4
Rinat <ringarifullin@gmail.com>
2016-03-03 22:34:21 +0300

    Decrease minimum length of author name for news

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,6 +2,6 @@ class News < ActiveRecord::Base
   has_many :newsusers
   has_many :users, through: :newsusers
 
-  validates :author, presence: true, length: { minimum: 5 }
+  validates :author, presence: true, length: { minimum: 3 }
   validates :text, presence: true, length: { minimum: 15 }
 end
