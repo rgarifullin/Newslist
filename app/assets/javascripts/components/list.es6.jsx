89059fff49a99d9f20fef3e048e2cb8163edf7ae
Rinat <ringarifullin@gmail.com>
2016-03-14 19:06:53 +0300

    Add change read status button

--- app/assets/javascripts/components/list.es6.jsx
+++ app/assets/javascripts/components/list.es6.jsx
@@ -32,13 +32,39 @@ class List extends React.Component {
 }
 
 class News extends React.Component {
+  handleClick(id, status) {
+    $.ajax({
+      url: '/news/' + id + '/change_status',
+      method: 'PATCH',
+      dataType: 'json',
+      success: function(statusData) {
+        $.ajax({
+          url: '/',
+          dataType: 'json',
+          success: function(data) {
+            let newState = this.props.post;
+            newState.status = statusData.status;
+            this.setState({post: newState});
+          }.bind(this),
+          error: function(xhr, status, err) {
+            console.error(this.props.url, status, err.toString());
+          }.bind(this)
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
+
   render() {
     let button;
     if (this.props.can_stats) {
+      let boundClick = this.handleClick.bind(this, this.props.post.news.id, this.props.post.status);
       if (this.props.post.status) {
-        button = <button className="btn btn-sm btn-default">Unread</button>;
+        button = <button className="btn btn-sm btn-default" onClick={boundClick}>Unread</button>;
       } else {
-        button = <button className="btn btn-sm btn-primary">Read</button>;
+        button = <button className="btn btn-sm btn-primary" onClick={boundClick}>Read</button>;
       }
     }
 
