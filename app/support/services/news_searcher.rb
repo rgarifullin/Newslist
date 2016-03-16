763bbeae49a9e8946377856bb53a6f357334142a
Rinat <ringarifullin@gmail.com>
2016-03-16 11:11:40 +0300

    Fix search service

--- app/support/services/news_searcher.rb
+++ app/support/services/news_searcher.rb
@@ -13,7 +13,7 @@ class Services::NewsSearcher
       query << condition_by_text(@params[:text])
     end
 
-    @list = News.where(query.join(' AND'))
+    @list = News.where(query.join(' AND '))
 
     condition_by_status(@params[:status])
   end
@@ -21,15 +21,15 @@ class Services::NewsSearcher
   private
 
   def condition_by_date(start_date, end_date)
-    start_date = DateTime.new if start_date.nil? || start_date.empty?
+    start_date = DateTime.new(0) if start_date.nil? || start_date.empty?
 
     end_date = DateTime.now.tomorrow if end_date.nil? || end_date.empty?
 
-    "created_at BETWEEN '#{start_date}' AND '#{end_date}'"
+    "(created_at BETWEEN '#{start_date}' AND '#{end_date}')"
   end
 
   def condition_by_text(text)
-    "author LIKE '%#{text}%' OR text LIKE '%#{text}%'"
+    "(author LIKE '%#{text}%' OR text LIKE '%#{text}%')"
   end
 
   def condition_by_status(status)
