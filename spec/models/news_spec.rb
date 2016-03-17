de88a8479de4ed18066492dff5433c99e676a494
Rinat <ringarifullin@gmail.com>
2016-03-17 11:33:51 +0300

    Refacor rspec tests

--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe News, :type => :model do
+RSpec.describe News, type: :model do
   let(:times_news) { FactoryGirl.create(:times_news) }
-  let(:rome_news) { FactoryGirl.create(:rome_news)}
+  let(:rome_news) { FactoryGirl.create(:rome_news) }
 
   it 'orders by author' do
     expect(News.order(:author)).to eq([rome_news, times_news])
@@ -10,7 +10,7 @@ RSpec.describe News, :type => :model do
 
   it 'dont save empty news' do
     empty_news = News.new()
-    
+
     expect(empty_news.valid?).to eq false
   end
 end
